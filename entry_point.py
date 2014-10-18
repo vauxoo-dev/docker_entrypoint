81fd06ed7dc8664fe10b1670060dbddc9d9451a9
nhomar <nhomar@vauxoo.com>
2014-10-18 02:04:05 -0430

    [IMP] Concepts of db_name, db_filter and data_dir added

--- entry_point.py
+++ entry_point.py
@@ -10,7 +10,7 @@ from shutil import copy2
 import pwd
 import fileinput
 
-FILESTORE_PATH = '/home/odoo/instance/odoo/openerp/filestore'
+DATA_DIR = '/home/odoo/filestore'
 CONFIGFILE_PATH = '/home/odoo/instance/config/odoo.conf'
 
 
@@ -54,11 +54,20 @@ def main():
     if getenv('DB_PORT'):
         change_value(CONFIGFILE_PATH, 'db_port', 'db_port = %s' % getenv('DB_PORT'))
 
+    if getenv('DB_NAME'):
+        change_value(CONFIGFILE_PATH, 'db_name', 'db_name = %s' % getenv('DB_NAME'))
+
+    if getenv('DB_FILTER'):
+        change_value(CONFIGFILE_PATH, 'db_filter', 'db_filter = %s' % getenv('DB_FILTER'))
+
+    if getenv('DATA_DIR'):
+        change_value(CONFIGFILE_PATH, 'data_dir', 'data_dir = %s' % getenv('DATA_DIR'))
+
     if get_owner(CONFIGFILE_PATH) != "odoo":
         call(["chown", "-R", "odoo", CONFIGFILE_PATH])
 
-    if get_owner(FILESTORE_PATH) != "odoo":
-        call(["chown", "-R", "odoo", FILESTORE_PATH])
+    if get_owner(DATA_DIR) != "odoo":
+        call(["chown", "-R", "odoo", DATA_DIR])
 
     call(["/usr/bin/supervisord"])
 
